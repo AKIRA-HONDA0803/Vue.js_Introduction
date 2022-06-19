2eed90b0f4d0f5a90bda21ddde77c39a5db73426
AkiraHonda <05410kira@gmail.com>
2022-06-19 16:06:27 +0900

    creteElement関数

---
+++ Chapter05/5.4.3/Column/jsx-counter/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'counter',
+  data: function() {
+    return {
+      counter: 0,
+    }
+  },
+  // eslint-disable-next-line
+  render: function (h) {
+    return <div>
+      <button
+        on-click={() => this.counter+=1}
+      >
+        クリックでカウントアップ
+      </button>
+      <p>
+        クリックされた回数：
+        <b>{this.counter}回</b>
+      </p>
+    </div>
+  }
+}
+</script>
