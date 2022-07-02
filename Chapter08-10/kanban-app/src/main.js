465e057e625ee872daf89e0d3444215d24bbe856
KRofLife <05410kira@gmail.com>
2022-07-02 14:14:03 +0900

    パフォーマンス測定の設定

--- Chapter08-10/kanban-app/src/main.js
+++ Chapter08-10/kanban-app/src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import 'es6-promise/auto'
+import 'es6-promise/auto' // プロミスをポリフィルする
 import App from './App'
-import ErrorBoundary from './ErrorBoundary.vue'
+import ErrorBoundary from './ErrorBoundary.vue' // エラーを補足するコンポーネント
 import router from './router'
-import store from './store'
+import store from './store' // Vuexのストアインスタンスをインポート
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true // NODE_ENV == 'development'で測定有効化
 
 // ErrorBoundaryコンポーネントのインストール
 Vue.component(ErrorBoundary.name, ErrorBoundary)
